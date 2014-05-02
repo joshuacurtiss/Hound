a33635ec901df9d9cb57505af302dfbde808ff59
Joshua Curtiss <j.curtiss@rockvalleycollege.edu>
2014-05-02 03:42:42 -0500

    Rough, initial support for a map on the address detail page.

--- Hound/addressDetailViewController.h
+++ Hound/addressDetailViewController.h
@@ -12,4 +12,5 @@
 @interface addressDetailViewController : UIViewController
 @property (strong, nonatomic) IBOutlet UILabel *addr1;
 @property (strong) NSManagedObject *address;
+@property (strong, nonatomic) IBOutlet MKMapView *mapview;
 @end
--- Hound/addressDetailViewController.m
+++ Hound/addressDetailViewController.m
@@ -9,13 +9,14 @@
 #import "addressDetailViewController.h"
 #import "addressEditViewController.h"
 #import "houndAppDelegate.h"
+#import <CoreLocation/CoreLocation.h>
 
 @interface addressDetailViewController ()
 @end
 
 @implementation addressDetailViewController
 
-@synthesize address;
+@synthesize address, mapview;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -37,12 +38,30 @@
     NSString *fullAddr=[NSString stringWithFormat:@"%@ %@ %@ %@ %@",[address valueForKey:@"addr1"],[address valueForKey:@"addr2"],[address valueForKey:@"city"],[address valueForKey:@"state"],[address valueForKey:@"zip"]];
     self.navigationItem.title=fullAddr;
     self.addr1.text=fullAddr;
+    CLLocationCoordinate2D coord=CLLocationCoordinate2DMake( [[address valueForKey:@"latitude"] doubleValue], [[address valueForKey:@"longitude"] doubleValue] );
+    [self showClusterPoint:coord withTitle:fullAddr];
+    mapview.showsUserLocation = YES;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(MKPointAnnotation *)showClusterPoint:(CLLocationCoordinate2D)coords withTitle:(NSString *)title
+{
+    float  zoomLevel = 0.03;
+    MKCoordinateRegion region = MKCoordinateRegionMake (coords, MKCoordinateSpanMake (zoomLevel, zoomLevel));
+    [mapview setRegion: [mapview regionThatFits: region] animated: YES];
+    
+    MKPointAnnotation *point = [[MKPointAnnotation alloc]init];
+    point.coordinate = coords;
+    point.title=title;
+    
+    [mapview addAnnotation:point];
+    
+    return point;
 }
 
 - (IBAction)unwindToTableViewController:(UIStoryboardSegue *)sender
